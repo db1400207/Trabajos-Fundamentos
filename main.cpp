8386cb354a125ec8a49fb14eb7e371828b5a10a2
db1400207 <jose.brenes.ortiz@est.una.ac.cr>
2026-02-23 14:58:17 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+	
+	cout << "    *"<<endl;
+	cout << "   **"<<endl;
+	cout << "  ***"<<endl;
+	cout << " ****"<<endl;
+	
+	cout << "\n****"<<endl;
+	cout << "***"<<endl;
+	cout << "**"<<endl;
+	cout << "*"<<endl;
+	
+	cout << "\n****"<<endl;
+	cout << "****"<<endl;
+	cout << "****"<<endl;
+	cout << "****"<<endl;
+	
+	return 0;
+}
+
