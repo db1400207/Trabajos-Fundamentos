ca1680478b7a07babad1ad89682fae517e4f451d
db1400207 <jose.brenes.ortiz@est.una.ac.cr>
2026-02-23 14:59:49 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,20 +3,24 @@ using namespace std;
 
 int main (int argc, char *argv[]) {
 	
-	cout << "    *"<<endl;
-	cout << "   **"<<endl;
-	cout << "  ***"<<endl;
-	cout << " ****"<<endl;
+	cout << "*******************"<<endl;
+	cout << "*                 *"<<endl;
+	cout << "*   Bienvenido!   *"<<endl;
+	cout << "*                 *"<<endl;
+	cout << "*******************";
 	
-	cout << "\n****"<<endl;
-	cout << "***"<<endl;
-	cout << "**"<<endl;
-	cout << "*"<<endl;
+	cout << "\n\n====================="<<endl;
+	cout << "||                 ||"<<endl;
+	cout << "||   Bienvenido!   ||"<<endl;
+	cout << "||                 ||"<<endl;
+	cout << "=====================";
 	
-	cout << "\n****"<<endl;
-	cout << "****"<<endl;
-	cout << "****"<<endl;
-	cout << "****"<<endl;
+	cout << "\n\n+---------+"<<endl;
+	cout << "|         |"<<endl;
+	cout << "|  Hola!  |"<<endl;
+	cout << "|         |"<<endl;
+	cout << "+---------+";
+		
 	
 	return 0;
 }
